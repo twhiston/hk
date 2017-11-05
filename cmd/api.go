434cbaf94a73853acb1f8e50350a03b4bf0fbd2c
Tom Whiston <tomw@nine.ch>
2017-11-05 15:55:59 +0100

    initial commit

---
+++ cmd/api.go
@@ -0,0 +1,53 @@
+package cmd
+
+type StatsResponse struct {
+	Overtime        string `json:"overtime"`
+	OvertimeSeconds int    `json:"overtime_in_seconds"`
+	Vacation struct {
+		Redeemed  float32 `json:"redeemed_days"`
+		Remaining float32 `json:"remaining_days"`
+	} `json:"vacation"`
+}
+
+type TimerResponse struct {
+	Date            string `json:"date"`
+	Start           string `json:"start_time"`
+	Duration        string `json:"duration"`
+	DurationSeconds int    `json:"duration_in_seconds"`
+	Note            string `json:"note"`
+	User struct {
+		Id    int    `json:"id"`
+		Name  string `json:"name"`
+		Teams []string `json:"teams"`
+	} `json:"user"`
+	Type struct {
+		Id       int    `json:"id"`
+		Name     string `json:"name"`
+		Type     string `json:"type"`
+		Excluded bool `json:"excluded_from_calculations"`
+	}
+	Project struct {
+		Id       int    `json:"id"`
+		Name     string `json:"name"`
+		Archived bool `json:"archived"`
+	}
+}
+
+type TimerTypesResponse []struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Type string `json:"type"`
+	Archived bool `json:"archived"`
+	Exclude bool `json:"exclude_from calculations"`
+}
+
+type TimerStartPayload struct {
+	Id int `json:"time_type_id"`
+	Start string `json:"start_time,omitempty"`
+	ProjectId string `json:"project_id,omitempty"`
+	Note string `json:"note,omitempty"`
+}
+
+type TimerStopPayload struct {
+	End string `json:"end_time,omitempty"`
+}
---
+++ cmd/helpers.go
@@ -0,0 +1,80 @@
+package cmd
+
+import (
+	"reflect"
+	"fmt"
+
+	"github.com/twhiston/clitable"
+	"github.com/spf13/viper"
+	"github.com/bndr/gopencils"
+	"net/http"
+	"log"
+)
+
+func GetApi() *gopencils.Resource {
+	domain := viper.GetString("hakuna.domain")
+	api := gopencils.Api("https://" + domain + ".hakuna.ch/api/v1/")
+	api.Headers = make(http.Header, 2)
+	api.Headers.Add("X-Auth-Token", viper.GetString("hakuna.token"))
+	api.Headers.Add("Content-Type", "application/json; charset=utf-8")
+	return api
+}
+
+func PrintResponse(resp interface{}) {
+	table := clitable.New()
+	//Get api response tags for the header
+	table.AddRow(getStructTags(resp)...)
+	//Get the data for the table body
+	table.AddRow(getStructVals(resp)...)
+	table.Print()
+}
+
+func PrintResponseArray(resp interface{}) {
+
+	tbl, _ := resp.([]interface{})
+	//if !err {
+	//	log.Fatalln("Response must be array for printing")
+	//}
+	table := clitable.New()
+	for k, v := range tbl {
+		if k == 0 {
+			table.AddRow(getStructTags(v)...)
+		}
+		table.AddRow(getStructVals(v)...)
+	}
+	table.Print()
+}
+
+func getStructVals(resp interface{}) []string {
+	data := make([]string, 0)
+	val := reflect.ValueOf(resp)
+	for i := 0; i < val.Type().NumField(); i++ {
+		if val.Type().Field(i).Type.Kind() == reflect.Struct {
+			a := getStructVals(val.Field(i).Interface())
+			data = append(data, a...)
+		} else {
+			data = append(data, fmt.Sprintf("%v", val.Field(i).Interface()))
+		}
+	}
+	return data
+}
+
+func getStructTags(resp interface{}) []string {
+	data := make([]string, 0)
+	val := reflect.ValueOf(resp)
+	for i := 0; i < val.Type().NumField(); i++ {
+		if val.Type().Field(i).Type.Kind() == reflect.Struct {
+			a := getStructTags(val.Field(i).Interface())
+			data = append(data, a...)
+		} else {
+			data = append(data, val.Type().Field(i).Tag.Get("json"))
+		}
+	}
+	return data
+}
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
